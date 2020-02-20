13828e481750b59c02c1967bd70944e28abf5d3e
Leandro Vieira <leandrown@outlook.com>
2020-02-20 11:31:20 -0300

    Template engine adicionada - Nunjucks

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const server = express();
+const nunjucks = require('nunjucks');
+
+// configurar o servidor para apresentar arquivos extras/estáticos
+server.use(express.static('public'));
+
+// configurar a template engine
+nunjucks.configure('./', { express: server });
 
 // configurar a apresentacao da pagina
 server.get('/', function(req, res) {
-   return res.send('Ok, cheguei aqui com o Nodemon');
+   return res.render('index.html');
 });
 
 // ligar o servidor e permitir o acesso na porta 3000
