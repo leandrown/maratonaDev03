b4e520101d9a997d1f6dde170bf62021064836c9
Leandro Vieira <leandrown@outlook.com>
2020-02-20 12:08:56 -0300

    Funcionalidade para pegar dados do form adicionada

--- server.js
+++ server.js
@@ -5,12 +5,46 @@ const nunjucks = require('nunjucks');
 // configurar o servidor para apresentar arquivos extras/estáticos
 server.use(express.static('public'));
 
+// habilitar o corpo/body do formulario
+server.use(express.urlencoded({ extended: true }));
+
 // configurar a template engine
-nunjucks.configure('./', { express: server });
+nunjucks.configure('./', { express: server, noCache: true });
+
+// Lista de doadores: Vetor ou Array
+const donors = [
+   {
+      name: "Adriana Vieira",
+      blouse: "Blusinha"
+   },
+   {
+      name: "Leandro Vieira",
+      blouse: "Blusa"
+   },
+   {
+      name: "Carlos Alberto",
+      blouse: "Calça Moletom"
+   },
+   {
+      name: "Solange Paulino",
+      blouse: "Blusa de Capuz"
+   }
+];
 
 // configurar a apresentacao da pagina
 server.get('/', function(req, res) {
-   return res.render('index.html');
+   return res.render('index.html', { donors });
+});
+
+server.post('/', function(req, res) {
+   // pegar dados do formulario
+   const name = req.body.name;
+   const email = req.body.email;
+   const blouse = req.body.blouse;
+
+   donors.push({ name, blouse });
+
+   return res.redirect('/');
 });
 
 // ligar o servidor e permitir o acesso na porta 3000
